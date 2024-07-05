0f0b5e1e9f9846d8694572bc4a08b7ce3a669052
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2024-07-05 19:59:36 -0300

    first commit

---
+++ exercicio26.ts
@@ -0,0 +1,14 @@
+function multiplicacao(num1:number, num2:number, resultado:number):number {
+    num1 = 5
+    num2 = 10
+    resultado = num1 * num2
+    return resultado
+}
+
+function saudacao(oi:string):string {
+    oi = "Olá, Pedro"
+    return oi
+}
+
+console.log(multiplicacao(1,2,3))
+console.log(saudacao("oi"))
